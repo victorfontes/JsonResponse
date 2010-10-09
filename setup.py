9b36980d99fde03df1e494b1800a6228dee54b14
Victor Fontes Costa <victor@t1000.local>
2010-10-09 04:50:15 -0300

    setup.py support

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+ 
+setup(name='json_response',
+    url='http://github.com/victorfontes/JsonResponse',
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Django',
+    ]
+)
